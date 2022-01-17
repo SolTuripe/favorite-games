9407034233bbdf0e6ebd5ae3705a2e50a75dc4d7
Sol <solcis.bio@gmail.com>
2022-01-17 21:39:01 +0100

    refactoring

---
+++ src/components/navbar.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/ico/logoj.png";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div class="ct-header">
+        <Link to="/">
+          <img className="img-logo" src={logo} alt="logo" />
+        </Link>
+        <Link to="/create" className={this.props.txtColor}>
+          CREATE
+        </Link>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
